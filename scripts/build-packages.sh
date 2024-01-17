fdd3499990c439f1803c9845b57d2795883cf8ce
ruthabebeneg <124071594+ruthabebeneg@users.noreply.github.com>
2024-01-17 11:23:50 +0100

    Update build-packages.sh

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -12,7 +12,8 @@ mkdir python
 pip3 install google-api-python-client --target python
 zip -r google-api-python-layer.zip python
 
-aws s3 cp google-api-python-layer.zip s3://your-bucket-name
+aws s3 cp google-api-python-layer.zip s3://textract-console-eu-west-3-70388abc-d446-447a-a281-45d5b235202b
+
 
 
 # zip the Lambda file and push to S3
@@ -21,4 +22,5 @@ cd ../src
 rm -f google-drive-lambda.zip
 zip google-drive-lambda.zip google-drive-lambda.py
 
-aws s3 cp google-drive-lambda.zip s3://your-bucket-name
+aws s3 cp google-drive-lambda.zip s3://textract-console-eu-west-3-70388abc-d446-447a-a281-45d5b235202b
+
