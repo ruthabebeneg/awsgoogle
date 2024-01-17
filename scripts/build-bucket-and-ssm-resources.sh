3dac4786ab6a9a3685e8aedf8328ec49921ac516
ruthabebeneg <124071594+ruthabebeneg@users.noreply.github.com>
2024-01-17 11:08:20 +0100

    Update build-bucket-and-ssm-resources.sh

--- scripts/build-bucket-and-ssm-resources.sh
+++ scripts/build-bucket-and-ssm-resources.sh
@@ -6,6 +6,7 @@ aws cloudformation create-stack \
     --stack-name google-drive-bucket-and-ssm \
     --template-body file://bucket-and-ssm-resources.yaml \
     --capabilities CAPABILITY_AUTO_EXPAND \
-    --parameters ParameterKey=LambdaAndReportBucket,ParameterValue=your-bucket-name \
+    --parameters ParameterKey=LambdaAndReportBucket,ParameterValue=textract-console-eu-west-3-70388abc-d446-447a-a281-45d5b235202b
+ \
         ParameterKey=GoogleServiceAccountCredentials,ParameterValue=google-service-account-credentials \
-        ParameterKey=GoogleDriveFolderId,ParameterValue=google-drive-folder-id
+        ParameterKey=GoogleDriveFolderId,ParameterValue=1YoRs3bswY7wSMMpJfGJYHV-ihgwfKUSX
