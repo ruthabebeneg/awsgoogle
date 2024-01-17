1abeacc35febd1c92f0e4d531f81d5358981b18c
ruthabebeneg <124071594+ruthabebeneg@users.noreply.github.com>
2024-01-17 11:24:02 +0100

    Update copy-test-files.sh

--- scripts/copy-test-files.sh
+++ scripts/copy-test-files.sh
@@ -2,4 +2,5 @@
 
 # before running this script modify the bucket name to the one created with build-bucket-and-ssm-resources.sh
 
-aws s3 cp ../testdata/ s3://your-bucket-name --recursive --exclude "*" --include "*.xlsx"
+aws s3 cp ../testdata/ s3://textract-console-eu-west-3-70388abc-d446-447a-a281-45d5b235202b
+ --recursive --exclude "*" --include "*.xlsx"
